adaf2a8fcf0a815c9d9fc75e3519ead8fa681a8e
hooni <jhlee909090@gmail.com>
2023-04-04 16:09:36 +0900

    HomeView UI 구성완료
    웰컴제주 사진 버튼 선택시 UI만 구현 완료

---
+++ WWDC.swiftpm/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  WWDC
+//
+//  Created by 이지훈 on 2023/04/04.
+//
+
+import UIKit
+
+class HomeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
